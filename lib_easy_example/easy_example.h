b7714aa11df251b49e316fda2c322560bd2b97ba
anvar <mr.nazyrov1997@mail.ru>
2024-10-14 04:04:54 +0300

    this is my 10th attempt to create a repository , but i managed and uploaded it all to the repository

---
+++ lib_easy_example/easy_example.h
@@ -0,0 +1,9 @@
+#pragma once
+// Copyright 2024 Marina Usova
+
+#ifndef LIB_EASY_EXAMPLE_EASY_EXAMPLE_H_
+#define LIB_EASY_EXAMPLE_EASY_EXAMPLE_H_
+
+float division(int a, int b);
+
+#endif  // LIB_EASY_EXAMPLE_EASY_EXAMPLE_H_
